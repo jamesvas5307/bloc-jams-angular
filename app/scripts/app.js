fe4de3a0ed161fd482c7a40ea99099ae2db21486
jamesvas5307 <jamesvas5307@live.com>
2017-05-16 07:06:24 -0500

    Checkpoint 3 Coursework

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-angular.module('blocJams',[])
+(function()){
+  function config($stateProvider,$locationProvider){
+    // To avoid hash bangs: you change enabled to true making clean urls
+    $locationProvider
+      .html5Mode({
+        enable: true,
+        requiredBase: false
+      });
+      // Basic Template: $stateProvider.state(stateName,stateConfig)
+      $stateProvider
+        .state('landing',{
+          url: '/',
+          templateUrl: 'templates/landing.html'
+        })
+
+        .state('album',{
+          url: '/',
+          templateUrl: 'templates/album.html'
+        });
+  }
+  angular
+        .module('blocJames'['ui.router'])
+        .config(config);
+
+  }

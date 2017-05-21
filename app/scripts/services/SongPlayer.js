19028575332d85277fade2c4cd87215a9e320043
James Vasquez <jamesvasquez@MacBook-Pro-6.local>
2017-05-21 12:06:36 -0400

    Checkpoint 7 Courseowkr

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -23,21 +23,34 @@
       });
       currentSong = song;
     };
-
+    /**
+      * @function PlaySong
+      * @desc Plays the currentBuzzObject and set the value to currentSong.playing to true
+    **/
+    var playSong = function(){
+        currentBuzzObject.play();
+        currentSong.playing = true;
+    };
+    /**
+      * @function songPlayer.play
+      * @desc Checks to see if the currentSong is not the song chosen: if not, sets the currentBuzzObject to song and then plays it.
+      * if true, checks to see if the song is paused, if it is, resume the song
+    **/
     songPlayer.play = function(song){
       if(currentSong !== song){
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong();
     } else if(currentSong == song){
         if(currentBuzzObject.isPaused()){
-          currentBuzzObject.play();
-          currentSong.playing = true;
+          playSong();
       }
     }
     };
-
-    songPlayer.pause = function(song){
+    /**
+      * @function songPlayer.pause
+      * @desc Method called on ng-click in HTML element to pause the song and set the song.playing attriute to false for determining conditional formatting in the player and list
+    **/
+      songPlayer.pause = function(song){
       currentBuzzObject.pause();
       song.playing = false;
     };
